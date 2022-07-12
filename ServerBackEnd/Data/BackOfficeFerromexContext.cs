312249ed248720115566f1c3c01b5f3d4cdcb7cc
Sergio Bejar <prosis.ssandoval@gmail.com>
2022-07-12 12:12:28 -0500

    Actualizacion de EF

--- ServerBackEnd/Data/BackOfficeFerromexContext.cs
+++ ServerBackEnd/Data/BackOfficeFerromexContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using ApiGateway;
+using ApiGateway.Models;
 using Shared;
 
 namespace ApiGateway.Data
@@ -22,6 +22,8 @@ namespace ApiGateway.Data
 
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<LogRole> LogRoles { get; set; }
+        public virtual DbSet<LogUserActivity> LogUserActivities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -36,6 +38,10 @@ namespace ApiGateway.Data
             {
                 entity.Property(e => e.Email).HasMaxLength(256);
 
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
                 entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
 
                 entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
@@ -43,6 +49,69 @@ namespace ApiGateway.Data
                 entity.Property(e => e.RefreshTokenExpiryTime).HasColumnType("datetime");
 
                 entity.Property(e => e.UserName).HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<LogRole>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.AspNetRolesId)
+                    .IsRequired()
+                    .HasMaxLength(450);
+
+                entity.Property(e => e.IdUser)
+                    .HasMaxLength(450)
+                    .HasColumnName("ID_User");
+
+                entity.Property(e => e.NewNameRol)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.OldNameRol)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TypeAction)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<LogUserActivity>(entity =>
+            {
+                entity.ToTable("LogUserActivity");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.AspNetRolesIdNew).HasMaxLength(450);
+
+                entity.Property(e => e.AspNetRolesIdOld).HasMaxLength(450);
+
+                entity.Property(e => e.IdModifiedUser)
+                    .HasMaxLength(450)
+                    .HasColumnName("ID_ModifiedUser");
+
+                entity.Property(e => e.IdUpdatedUser)
+                    .IsRequired()
+                    .HasMaxLength(450)
+                    .HasColumnName("ID_UpdatedUser");
+
+                entity.Property(e => e.NewLastName).HasMaxLength(60);
+
+                entity.Property(e => e.NewName).HasMaxLength(60);
+
+                entity.Property(e => e.OldLastName).HasMaxLength(60);
+
+                entity.Property(e => e.OldName).HasMaxLength(60);
+
+                entity.Property(e => e.TypeAction)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
             });
 
             OnModelCreatingPartial(modelBuilder);
